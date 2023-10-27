e2fdefbddb607b5096613722516981a59d02516f
Adam <dziuketta@gmail.com>
2023-10-27 10:07:32 +0200

    First commit

---
+++ popup/popup.js
@@ -0,0 +1,28 @@
+const CONFIG_CHARACTERS = ['#', 'later', 'rmd']
+
+chrome.bookmarks.getTree((res) => {
+  const allBookmarks = res[0].children[0].children
+  const savedForLater = []
+  allBookmarks.map((bookmark) => {
+    const { title } = bookmark
+    const result = CONFIG_CHARACTERS.some((i) => title.includes(i))
+    if (result) {
+      savedForLater.push(bookmark)
+    }
+  })
+  savedForLater.map((bookmark) => {
+    console.log(bookmark)
+    const tr = document.createElement('tr')
+    const tdName = document.createElement('td')
+    const tdDate = document.createElement('td')
+    const name = document.createTextNode(bookmark.title)
+    const date = document.createTextNode(
+      new Date(bookmark.dateAdded).toLocaleString()
+    )
+    tdName.appendChild(name)
+    tdDate.appendChild(date)
+    tr.appendChild(tdName)
+    tr.appendChild(tdDate)
+    document.querySelector('table').appendChild(tr)
+  })
+})
---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CONFIG_CHARACTERS = ['#', 'later', 'rmd']
+
+function notifyUser(savedForLater) {
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: './images/icon.png',
+    title: 'TabReminder',
+    message: `Number of saved for later bookmarks: ${savedForLater.length}`,
+  })
+}
+
+chrome.bookmarks.onChanged.addListener((id, changeInfo) => {
+  chrome.bookmarks.getTree((res) => {
+    const allBookmarks = res[0].children[0].children
+    const savedForLater = []
+    allBookmarks.map((bookmark) => {
+      const { title } = bookmark
+      const result = CONFIG_CHARACTERS.some((i) => title.includes(i))
+      if (result) {
+        savedForLater.push(bookmark)
+      }
+    })
+    notifyUser(savedForLater)
+  })
+})
